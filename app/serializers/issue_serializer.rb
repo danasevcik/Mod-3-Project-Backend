e87a9157b74dcde4a040525ccda0b3fbc69a60ce
Ethan Nam <ethan.s.nam@gmail.com>
2019-03-25 17:35:03 -0400

    Sorted issues before sending json back.

--- app/serializers/issue_serializer.rb
+++ app/serializers/issue_serializer.rb
@@ -1,3 +1,3 @@
 class IssueSerializer < ActiveModel::Serializer
-  attributes :id, :title, :zipcode, :description, :resolved, :category_id
+  attributes :id, :title, :zipcode, :description, :resolved, :category_id, :votes
 end
